b0ebc160f468851691841a6783bd5a2d18f1560d
asmaaEssam <61593032+asmaaEssam@users.noreply.github.com>
2020-09-14 15:42:52 +0200

    creating replying to a message function

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -3,177 +3,247 @@ const ObjectID = require("mongodb").ObjectID;
 
 const isAuthenticated = require("../helpers/authentication").isAuthenticated;
 
-
 //Posting Message API  ==>> "/message/add"
 function postMsg(req, res, db) {
-    if (req.method === "POST") {
+  if (req.method === "POST") {
+    //check authentication
+    const token = req.headers["token"];
+    const _id = isAuthenticated(token);
+    if (!_id) {
+      res.writeHead(404);
+      res.end("user is not authenticated");
+    } else {
+      let decorder = new StringDecoder("utf-8");
+      let buffer = "";
+
+      req.on("data", (chunck) => {
+        buffer += decorder.write(chunck);
+      });
+
+      req.on("end", async () => {
+        buffer += decorder.end();
+        const newMsg = JSON.parse(buffer);
+        if (!newMsg.body) {
+          res.writeHead(404);
+          res.end("Message body is required");
+        } else {
+          newMsg.userId = _id;
+          try {
+            await db.collection("messages").insertOne(newMsg);
+            console.log("1 document inserted");
+            res.writeHead(200, "ok", { "content-type": "application/json" });
+            console.log(buffer);
+            res.end(buffer);
+          } catch (err) {
+            throw new Error(err);
+          }
+        }
+      });
+    }
+  } else {
+    res.writeHead(404);
+    res.end("Request Method is not valid");
+  }
+}
+
+//Editing Message API ==>> "/message/edit/:id"
+function editMsg(req, res, db) {
+  if (req.method === "POST" || req.method === "PATCH") {
+    //TODO change it in production and deployment
+
+    const myURL = new URL("http://localhost:9000" + req.url);
+    const msgId = myURL.searchParams.get("id");
+    //check authentication
+    const token = req.headers["token"];
+    const userId = isAuthenticated(token);
+
+    if (!userId) {
+      res.writeHead(404);
+      res.end("user is not authenticated");
+    } else {
+      let decorder = new StringDecoder("utf-8");
+      let buffer = "";
+
+      req.on("data", (chunck) => {
+        buffer += decorder.write(chunck);
+      });
+
+      req.on("end", async () => {
+        buffer += decorder.end();
+        const msgUpdated = JSON.parse(buffer);
+
+        const msg = await db
+          .collection("messages")
+          .findOne({ _id: new ObjectID(msgId) });
+
+        if (msg) {
+          if (msg.userId !== userId) {
+            res.writeHead(404);
+            res.end("User is not authenticated");
+          } else {
+            if (!msgUpdated.body) {
+              res.writeHead(404);
+              res.end("Message body is required");
+            } else {
+              try {
+                await db
+                  .collection("messages")
+                  .findOneAndUpdate(
+                    { _id: new ObjectID(msgId) },
+                    { $set: { body: msgUpdated.body } }
+                  );
+
+                console.log("1 document updated");
+                res.writeHead(200, "ok", {
+                  "content-type": "application/json",
+                });
+                res.end(buffer);
+              } catch (err) {
+                throw new Error(err);
+              }
+            }
+          }
+        } else {
+          res.writeHead(404);
+          res.end("Message Not Found");
+        }
+      });
+    }
+  } else {
+    res.writeHead(404);
+    res.end("Request Method is not valid");
+  }
+}
+
+//Deleting Message API == >> "/message/delete/:id"
+async function deleteMsg(req, res, db) {
+  if (req.method === "DELETE") {
+    //TODO change it in production and deployment
+
+    const myURL = new URL("http://localhost:9000" + req.url);
+    const msgId = myURL.searchParams.get("id");
+
+    if (msgId === null || msgId.length !== 24) {
+      res.writeHead(404);
+      res.end("Message Not Found");
+    } else {
       //check authentication
       const token = req.headers["token"];
-      const _id = isAuthenticated(token);
-      if (!_id) {
+      const userId = isAuthenticated(token);
+
+      if (!userId) {
         res.writeHead(404);
         res.end("user is not authenticated");
       } else {
-        let decorder = new StringDecoder("utf-8");
-        let buffer = "";
-  
-        req.on("data", (chunck) => {
-          buffer += decorder.write(chunck);
-        });
-  
-        req.on("end", async () => {
-          buffer += decorder.end();
-          const newMsg = JSON.parse(buffer);
-          if (!newMsg.body) {
+        const msg = await db
+          .collection("messages")
+          .findOne({ _id: new ObjectID(msgId) });
+        if (msg) {
+          if (msg.userId !== userId) {
             res.writeHead(404);
-            res.end("Message body is required");
+            res.end("User is not authenticated");
           } else {
-            newMsg.userId = _id;
             try {
-              await db.collection("messages").insertOne(newMsg);
-              console.log("1 document inserted");
-              res.writeHead(200, "ok", { "content-type": "application/json" });
-              console.log(buffer)
+              await db
+                .collection("messages")
+                .findOneAndDelete({ _id: new ObjectID(msgId) });
+
+              console.log("1 document Deleted");
+              res.writeHead(200, "ok", {
+                "content-type": "application/json",
+              });
+              res.end("Message Deleted Successfully");
+            } catch (err) {
+              throw new Error(err);
+            }
+          }
+        } else {
+          res.writeHead(404);
+          res.end("Message Not Found");
+        }
+      }
+    }
+  } else {
+    res.writeHead(404);
+    res.end("Request Method is not valid");
+  }
+}
+
+//Replying on Message API == >> "/message/reply/:id"
+async function msgReply(req, res, db) {
+  if (req.method === "POST" || req.method === "PATCH") {
+    //TODO change it in production and deployment
+
+    const myURL = new URL("http://localhost:9000" + req.url);
+    const msgId = myURL.searchParams.get("id");
+    //check authentication
+    const token = req.headers["token"];
+    const userId = isAuthenticated(token);
+    const user = await db
+      .collection("users")
+      .findOne({ _id: new ObjectID(userId) });
+
+    if (!user) {
+      res.writeHead(404);
+      res.end("user is not authenticated");
+    } else {
+      let decorder = new StringDecoder("utf-8");
+      let buffer = "";
+
+      req.on("data", (chunck) => {
+        buffer += decorder.write(chunck);
+      });
+
+      req.on("end", async () => {
+        buffer += decorder.end();
+        const msgUpdated = JSON.parse(buffer);
+
+        const msg = await db
+          .collection("messages")
+          .findOne({ _id: new ObjectID(msgId) });
+
+        if (msg) {
+          if (!msgUpdated.reply.body) {
+            res.writeHead(404);
+            res.end("Reply body is required");
+          } else {
+            try {
+              await db.collection("messages").findOneAndUpdate(
+                { _id: new ObjectID(msgId) },
+                {
+                  $set: {
+                    replies: {
+                      userId: new ObjectID(userId),
+                      reply: msgUpdated.reply.body,
+                    },
+                  },
+                }
+              );
+
+              console.log("1 reply is added");
+              res.writeHead(200, "ok", {
+                "content-type": "application/json",
+              });
               res.end(buffer);
             } catch (err) {
               throw new Error(err);
             }
           }
-        });
-      }
-    } else {
-      res.writeHead(404);
-      res.end("Request Method is not valid");
-    }
-  }
-
-  //Editing Message API ==>> "/message/edit/:id"
-function editMsg(req, res, db) {
-    if (req.method === "POST" || req.method === "PATCH") {
-      //TODO change it in production and deployment
-  
-      const myURL = new URL("http://localhost:9000" + req.url);
-      const msgId = myURL.searchParams.get("id");
-      //check authentication
-      const token = req.headers["token"];
-      const userId = isAuthenticated(token);
-  
-      if (!userId) {
-        res.writeHead(404);
-        res.end("user is not authenticated");
-      } else {
-        let decorder = new StringDecoder("utf-8");
-        let buffer = "";
-  
-        req.on("data", (chunck) => {
-          buffer += decorder.write(chunck);
-        });
-  
-        req.on("end", async () => {
-          buffer += decorder.end();
-          const msgUpdated = JSON.parse(buffer);
-  
-          const msg = await db
-            .collection("messages")
-            .findOne({ _id: new ObjectID(msgId) });
-  
-          if (msg) {
-            if (msg.userId !== userId) {
-              res.writeHead(404);
-              res.end("User is not authenticated");
-            } else {
-              if (!msgUpdated.body) {
-                res.writeHead(404);
-                res.end("Message body is required");
-              } else {
-                try {
-                  await db
-                    .collection("messages")
-                    .findOneAndUpdate(
-                      { _id: new ObjectID(msgId) },
-                      { $set: { body: msgUpdated.body } }
-                    );
-  
-                  console.log("1 document updated");
-                  res.writeHead(200, "ok", {
-                    "content-type": "application/json",
-                  });
-                  res.end(buffer);
-                } catch (err) {
-                  throw new Error(err);
-                }
-              }
-            }
-          } else {
-            res.writeHead(404);
-            res.end("Message Not Found");
-          }
-        });
-      }
-    } else {
-      res.writeHead(404);
-      res.end("Request Method is not valid");
-    }
-  }
-
-
-  //Deleting Message API == >> "/message/delete/:id"
-async function deleteMsg(req, res, db) {
-    if (req.method === "DELETE") {
-      //TODO change it in production and deployment
-  
-      const myURL = new URL("http://localhost:9000" + req.url);
-      const msgId = myURL.searchParams.get("id");
-  
-      if (msgId === null || msgId.length !== 24) {
-        res.writeHead(404);
-        res.end("Message Not Found");
-      } else {
-        //check authentication
-        const token = req.headers["token"];
-        const userId = isAuthenticated(token);
-  
-        if (!userId) {
-          res.writeHead(404);
-          res.end("user is not authenticated");
         } else {
-          const msg = await db
-            .collection("messages")
-            .findOne({ _id: new ObjectID(msgId) });
-          if (msg) {
-            if (msg.userId !== userId) {
-              res.writeHead(404);
-              res.end("User is not authenticated");
-            } else {
-              try {
-                await db
-                  .collection("messages")
-                  .findOneAndDelete({ _id: new ObjectID(msgId) });
-  
-                console.log("1 document Deleted");
-                res.writeHead(200, "ok", {
-                  "content-type": "application/json",
-                });
-                res.end("Message Deleted Successfully");
-              } catch (err) {
-                throw new Error(err);
-              }
-            }
-          } else {
-            res.writeHead(404);
-            res.end("Message Not Found");
-          }
+          res.writeHead(404);
+          res.end("Message Not Found");
         }
-      }
-    } else {
-      res.writeHead(404);
-      res.end("Request Method is not valid");
+      });
     }
+  } else {
+    res.writeHead(404);
+    res.end("Request Method is not valid");
   }
+}
 
-  module.exports = {
-    postMsg,
-    editMsg,
-    deleteMsg,
-
-  };
+module.exports = {
+  postMsg,
+  editMsg,
+  deleteMsg,
+  msgReply
+};
