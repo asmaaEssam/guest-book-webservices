8e1c8c869731be113147b521ca2291d78d62ba58
asmaaEssam <61593032+asmaaEssam@users.noreply.github.com>
2020-09-14 15:33:04 +0200

    creating editing message function

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -45,3 +45,77 @@ function postMsg(req, res, db) {
       res.end("Request Method is not valid");
     }
   }
+
+  //Editing Message API ==>> "/message/edit/:id"
+function editMsg(req, res, db) {
+    if (req.method === "POST" || req.method === "PATCH") {
+      //TODO change it in production and deployment
+  
+      const myURL = new URL("http://localhost:9000" + req.url);
+      const msgId = myURL.searchParams.get("id");
+      //check authentication
+      const token = req.headers["token"];
+      const userId = isAuthenticated(token);
+  
+      if (!userId) {
+        res.writeHead(404);
+        res.end("user is not authenticated");
+      } else {
+        let decorder = new StringDecoder("utf-8");
+        let buffer = "";
+  
+        req.on("data", (chunck) => {
+          buffer += decorder.write(chunck);
+        });
+  
+        req.on("end", async () => {
+          buffer += decorder.end();
+          const msgUpdated = JSON.parse(buffer);
+  
+          const msg = await db
+            .collection("messages")
+            .findOne({ _id: new ObjectID(msgId) });
+  
+          if (msg) {
+            if (msg.userId !== userId) {
+              res.writeHead(404);
+              res.end("User is not authenticated");
+            } else {
+              if (!msgUpdated.body) {
+                res.writeHead(404);
+                res.end("Message body is required");
+              } else {
+                try {
+                  await db
+                    .collection("messages")
+                    .findOneAndUpdate(
+                      { _id: new ObjectID(msgId) },
+                      { $set: { body: msgUpdated.body } }
+                    );
+  
+                  console.log("1 document updated");
+                  res.writeHead(200, "ok", {
+                    "content-type": "application/json",
+                  });
+                  res.end(buffer);
+                } catch (err) {
+                  throw new Error(err);
+                }
+              }
+            }
+          } else {
+            res.writeHead(404);
+            res.end("Message Not Found");
+          }
+        });
+      }
+    } else {
+      res.writeHead(404);
+      res.end("Request Method is not valid");
+    }
+  }
+
+  module.exports = {
+    postMsg,
+    editMsg,
+  };
